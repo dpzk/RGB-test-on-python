3fc99d2d60a4e58318bb5a68f39716407a3efd79
icreiuheciijc <thomas.zhu.sh@hotmail.com>
2018-05-01 11:46:26 +0800

    Change

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 # import tkinter
 from tkinter import *
+from time import sleep
+from os import path
+from sys import exit
 
 # build window on tkinter
 tk = Tk()
 tk.wm_attributes('-fullscreen', True)
 tk.resizable(False, False)
+tk.title('')
+icon = PhotoImage(width = 16, height = 16)
+icon.blank()
+tk.call('wm', 'iconphoto', tk._w, icon)
 tk.update()
-canvas = Canvas(tk, width = tk.winfo_width(), height = tk.winfo_height(), bd = 0, highlightthickness = 0)
+canvas = Canvas(tk, width = tk.winfo_width(), height = tk.winfo_height(), bd = 0, highlightthickness = 0, cursor = 'none')
 canvas.pack()
 background = canvas.create_rectangle(0, 0, tk.winfo_width(), tk.winfo_height(), fill = '#FF0000', outline = '')
 
 # define refresh function
 def load(R, G, B):
-    canvas.itemconfig(background, fill = '#%02X%02X%02X' % (R, G, B))
-    tk.update_idletasks()
-    tk.update()
+    try:
+        canvas.itemconfig(background, fill = '#%02X%02X%02X' % (R, G, B))
+        tk.update_idletasks()
+        tk.update()
+        sleep(0.01)
+    except:
+        exit(0)
 
 # main loop
 while True:
